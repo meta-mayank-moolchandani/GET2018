dcfffff6ee3590bfc8da98ee26d7582b774332d8
meta-mayank-moolchandani <41181672+meta-mayank-moolchandani@users.noreply.github.com>
2018-08-30 15:01:02 +0530

    EAD/session3

---
+++ session3/src/com/servlets/AddEmployee.java
@@ -0,0 +1,73 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.Dao;
+import com.dao.EmpDao;
+import com.dao.Employee;
+
+/**
+ * Servlet implementation class AddEmployee
+ */
+
+public class AddEmployee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddEmployee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html" + "");
+
+		String firstName = request.getParameter("first_name");
+		String lastName = request.getParameter("last_name");
+		String email = request.getParameter("email");
+		String age = request.getParameter("age");
+
+		PrintWriter out = response.getWriter();
+		Employee employee = new Employee(firstName, lastName, email,
+				Integer.parseInt(age));
+		Dao<Employee>empDao = new EmpDao();
+		boolean isEmployeeAddedflag = empDao.insert(employee);
+
+		if (isEmployeeAddedflag) {
+			out.println("<h2>Employee Successfully Added !!</h2>");
+
+			RequestDispatcher requestdispatch = request
+					.getRequestDispatcher("index.html");
+			requestdispatch.include(request, response);
+		} else {
+			out.println("<h2>Employee id not Added! employee already exist ! </h2><a href = 'index.html'>home</a>");
+
+		}
+
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
