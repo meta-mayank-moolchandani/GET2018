0fad6426b82555796dda02c0c192ded1f219753d
meta-mayank-moolchandani <41181672+meta-mayank-moolchandani@users.noreply.github.com>
2018-09-03 13:07:53 +0530

    sql updated

--- Session4/sqlFile.sql
+++ Session4/sqlFile.sql
@@ -15,4 +15,9 @@ org_name VARCHAR(100),
 image_url VARCHAR(100) DEFAULT './images/dummy.jpg'
 );
 
-drop database user_details;
+INSERT INTO user_info(first_name, last_name,age,dob,mobile_number,email,password,org_name) VALUES ('dummy_user1','dummy',5,'1997-08-10',1234567890,'abc@abc.com','abcd','metacube');
+INSERT INTO user_info(first_name, last_name,age,dob,mobile_number,email,password,org_name) VALUES ('dummy_user2','dummy',6,'1997-08-10',1234567890,'abc@abcd.com','abcd','metacube');
+INSERT INTO user_info(first_name, last_name,age,dob,mobile_number,email,password,org_name) VALUES ('dummy_user3','dummy',7,'1997-08-10',1234567890,'abc@abcde.com','abcd','metacube');
+INSERT INTO user_info(first_name, last_name,age,dob,mobile_number,email,password,org_name) VALUES ('dummy_user4','dummy',8,'1997-08-10',1234567890,'abc@abcsde.com','abcd','metacube');
+INSERT INTO user_info(first_name, last_name,age,dob,mobile_number,email,password,org_name) VALUES ('dummy_user5','dummy',58,'1997-08-10',1234567890,'abc@abcdefg.com','abcd','google');
+
