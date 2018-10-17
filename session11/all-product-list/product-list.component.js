ba0ac6c2a9d2cb77a57411b7e3ffdcae8fe85fe1
meta-mayank-moolchandani <41181672+meta-mayank-moolchandani@users.noreply.github.com>
2018-10-17 20:54:56 +0530

    ui s11

--- session11/all-product-list/product-list.component.js
+++ session11/all-product-list/product-list.component.js
@@ -1,14 +1,56 @@
 'use strict';
 
-
-
-
 angular
     .module('product-list')
     .component('productList',{
             templateUrl:'all-product-list/product-list-template.html',
-            controller:function productListController($scope,$http){
+            controller:function productListController($http,$rootScope){
                 let url = 'http://localhost:3000';
+                $rootScope.totalItemsInCart;
+                
+                var self = this;
+
+                $http.get(url+"/cart").then(function(response){
+                    $rootScope.totalItemsInCart = response.data.length;
+                });
+
+                $http.get(url+"/cart?productId="+productId).then(function(response){
+                    self.isItemAdded = response.data.length;
+                });
+
+
+                this.addItemsToCart = function(product){
+                    $http.get(url+'/cart?productId='+product.id).then(function(response){
+                        if(response.data.length===0){
+                            $http({
+                                method:'POST',
+                                url:url+"/cart",
+                                data:{
+                                    productId: product.id,
+                                    productQuantity: 1
+                                },
+                                dataType:'json'
+                            });
+                        }else{
+                            $http({
+                                method:'PUT',
+                                url:url+"/cart/"+response.data[0].id,
+                                data:{
+                                    id:response.data.id,
+                                    productId: product.id,
+                                    productQuantity: response.data[0].productQuantity+1
+                                },
+                                dataType:'json'
+                            });
+                        }
+                   });
+                    window.location.reload();
+                };
+
+
+
+                console.log("pc"+ $rootScope.totalItemsInCart);
+
 
                 var self = this;
                 $http.get(url+'/all').then(function(response){
--- session11/all-product-list/product-list.module.js
+++ session11/all-product-list/product-list.module.js
@@ -1,6 +1,3 @@
 'use strict';
-
-
-
 angular
     .module('product-list',[]);
